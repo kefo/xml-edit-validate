be03764f18549eeff1c5d9d60fe4271212cef5c9
Kevin Ford <kefo@3windmills.com>
2013-08-04 18:58:02 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,39 @@
+// imports
+var http = require('http'),
+    path = require('path'),
+    express = require('express');
+
+// establish server
+var express = require('express');
+var server = express();
+
+// configure application
+server.configure(function () {
+  server.set('views', __dirname + '/views');
+  server.set('view engine', 'jade');
+});
+
+server.configure('development', function () {
+  server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  server.use(express.static(__dirname + '/public_html'));
+});
+
+server.configure('production', function () {
+  server.use(express.errorHandler());
+  server.use(express.static(__dirname + '/public_html', {maxAge: 60*15*1000}));
+});
+
+server.get('/', function (req, res){
+  res.render('index', {
+    title: 'XML Edit and Validate'
+  });
+});
+
+server.get('/about/', function (req, res){
+  res.render('about', {
+    title: 'about xml edit and validate'
+  });
+});
+
+server.listen(3000);
+console.log('Server started; Listening on port 3000');
